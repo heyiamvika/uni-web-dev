ac5ea564b838bbe695810de9bc0569c4e978deeb
Viktoriia Nosova <viktoriianosova@hotmail.com>
2019-11-04 13:09:18 +0200

    About page finished

--- src/about/about.js
+++ src/about/about.js
@@ -1,30 +1,61 @@
 const achievments = [
 	{
 		title: 'projects completed',
-		img: '',
+		img: '../../assets/briefcase.svg',
 		count: 548,
 	},
 	{
 		title: 'WORKING HOURS',
-		img: '',
+		img: '../../assets/history.svg',
 		count: 1465,
 	},
 	{
 		title: 'POSITIVE FEEDBACKS',
-		img: '',
+		img: '../../assets/star.svg',
 		count: 612,
 	},
 	{
 		title: 'HAPPY CLIENTS',
-		img: '',
+		img: '../../assets/heart.svg',
 		count: 735,
 	},
 ];
 
 appendAchievments();
+addButtonEvent();
 
 function appendAchievments() {
+	const parent = document.getElementById('about-achievments');
+
 	achievments.forEach(item => {
-		console.log(item);
+		const box = document.createElement('div');
+		box.className = 'box';
+		const imageBox = document.createElement('div');
+		imageBox.className = 'image-box';
+		const textBox = document.createElement('div');
+		textBox.className = 'text-box';
+		const image = document.createElement('img');
+		const count = document.createElement('span');
+		count.className = 'text-box-count';
+		const text = document.createElement('span');
+		text.className = 'text-box-text';
+
+		count.innerHTML = String(Math.round(item.count));
+		text.innerHTML = item.title;
+		image.src = item.img;
+
+		imageBox.appendChild(image);
+		textBox.appendChild(count);
+		textBox.appendChild(text);
+
+		box.appendChild(imageBox);
+		box.appendChild(textBox);
+
+		parent.appendChild(box);
 	});
+}
+
+function addButtonEvent() {
+	const button = document.getElementById('contact-button');
+	button.onclick = event => (window.location.href = '../contact/contact.html');
 }
