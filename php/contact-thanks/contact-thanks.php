a2f86dade9babd2d0eeb5171244362670356802f
Viktoriia Nosova <viktoriianosova@hotmail.com>
2019-11-12 16:47:44 +0200

    Contact form php

---
+++ php/contact-thanks/contact-thanks.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta charset="utf-8" />
+	<title>Основи WEB-програмування - Thank you!</title>
+	<link rel="stylesheet" href="../../src/index.css" />
+	<link rel="stylesheet" href="./contact-thanks.css" />
+	<link rel="stylesheet" href="./contact-thanks-responsive.css" />
+</head>
+
+<body>
+	<header>
+		<img id="home-logo" src="../../assets/code.svg" />
+		<nav>
+			<a href="../../src/index.html" class="nav-text">Home</a>
+			<a href="../../src/about/about.html" class="nav-text">About</a>
+			<a href="../../src/contact/contact.html" id="focused" class="nav-text">Contact</a>
+		</nav>
+	</header>
+
+	<main>
+		<div id="wrapper">
+			<h1 class="simple-text-heading ">Thank you for your comment!</h1>
+			<p class="simple-text">You submitted the form with the following info:</p>
+			<?=
+				"<div id=\"submitted-info\">
+					<p id=\"submitted-name\">
+						<span class=\"simple-text-heading\">Name:</span>
+						<span class=\"simple-text\">{$_POST['user-name']}</span>
+					</p>
+					<p id=\"submitted-email\">
+						<span class=\"simple-text-heading\">Email:</span>
+						<span class=\"simple-text\">{$_POST['user-email']}</span>
+					</p>
+					<p id=\"submitted-title\">
+					   <span class=\"simple-text-heading\">Title:</span>
+					   <span class=\"simple-text\">{$_POST['user-title']}</span>
+				  	</p>
+				    <p id=\"submitted-comment\">
+						<span class=\"simple-text-heading\">Comment:</span>
+						<span class=\"simple-text\">{$_POST['user-comment']}</span>
+			 	    </p>
+				</div>"
+			?>
+			<button class="green-button" id="back-home-button">
+				<a href="../../src/index.html">Back home</a>
+			</button>
+		</div>
+	</main>
+
+	<footer>
+		<div class="footer-box footer-credentials">
+			<h2 id="company-name">Vika Nosova, Inc.</h2>
+			<p>
+				Icons made by
+				<a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>
+				from
+				<a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+			</p>
+		</div>
+		<div class="footer-box footer-contacts">
+			<p id="footer-contacts-mail"><a href="mailto:@viktoriianosova@hotmail.com">viktoriianosova@hotmail.com</a></p>
+			<p id="footer-contacts-phone"><a href="tel: +380959105046">+380959105046</a></p>
+		</div>
+		<div class="footer-box footer-sitemap">
+			<ul>
+				<div class="group">
+					<li><a>Projects</a></li>
+					<li><a>About</a></li>
+					<li><a>Services</a></li>
+					<li><a>Carreer</a></li>
+				</div>
+				<div class="group">
+					<li><a>News</a></li>
+					<li><a>Events</a></li>
+					<li><a>Contact</a></li>
+					<li><a>Legals</a></li>
+				</div>
+				<div class="group">
+					<li><a>Facebook</a></li>
+					<li><a>Twitter</a></li>
+					<li><a>Instagram</a></li>
+					<li><a>Dribbble</a></li>
+				</div>
+			</ul>
+		</div>
+	</footer>
+	<!-- <script src="./contact.js"></script> -->
+</body>
+
+</html>
