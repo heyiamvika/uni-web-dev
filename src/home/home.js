d3ec2fdddacd25162022041cda1bbdaba23e311c
Viktoriia Nosova <viktoriianosova@hotmail.com>
2019-11-13 16:16:39 +0200

    Redirect

--- src/home/home.js
+++ src/home/home.js
@@ -1,6 +1,6 @@
-addHovers();
+addHoversAndEvents();
 
-function addHovers() {
+function addHoversAndEvents() {
 	const wrapper = document.getElementsByClassName('project-wrapper');
 	const newProjectHover = document.getElementsByClassName('project-hover');
 
@@ -17,9 +17,14 @@ function addHovers() {
 			newProjectHover[index].style.display = 'none';
 		}
 
+		function mouseClick(index) {
+			window.location = `./project/project.html?index=${index}`;
+		}
+
 		for (let i = 0; i < wrapper.length; i++) {
 			wrapper[i].addEventListener('mouseover', event => mouseOver(i));
 			wrapper[i].addEventListener('mouseleave', event => mouseLeave(i));
+			wrapper[i].addEventListener('click', event => mouseClick(i));
 		}
 	}
 }
---
+++ src/home/home.php
@@ -0,0 +1,49 @@
+<?php
+
+$elementsPerRow = 4;
+// Get the content of the JSON file
+$json = file_get_contents('../projects.json');
+// decode the JSON into an associative array
+$decoded = json_decode($json, true);
+$projects = $decoded['projects'];
+$projectsCount = count($projects);
+
+$rowsCount = $projectsCount % $elementsPerRow === 0
+    ? $projectsCount / $elementsPerRow
+    : floor($projectsCount / $elementsPerRow) + 1;
+
+for ($i = 0; $i < $rowsCount; $i++) {
+    $isRowEven = $i % 2 === 0;
+
+    echo "<div id=\"grid-row\">";
+
+    for ($j = $i * $elementsPerRow; $j < ($i + 1) * $elementsPerRow; $j++) {
+        $isElementEven = ($j + 1) % 2 === 0;
+
+        echo "<div class=\"project-wrapper\">";
+
+        if (($isRowEven && $isElementEven) || (!$isRowEven && !$isElementEven)) {
+            echo "<div class=\"project box-dark\">
+							<img class=\"project-img\" src=\"{$projects[$j]['img']}\">
+							<div class=\"project-opacity opacity-dark\"></div>
+						</div>";
+
+            echo "<div class=\"project-hover\">
+							<img class=\"project-hover-img\" src=\"../assets/code-hover.svg\">
+						</div>";
+        } else {
+            echo "<div class=\"project box-light\">
+							<img class=\"project-img\" src=\"{$projects[$j]['img']}\">
+							<div class=\"project-opacity opacity-light\"></div>
+						</div>";
+
+            echo "<div class=\"project-hover\">
+						<img class=\"project-hover-img\" src=\"../assets/code-hover.svg\"/>
+						</div>";
+        }
+
+        echo "</div>";
+    }
+
+    echo "</div>";
+}
